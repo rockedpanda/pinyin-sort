c3831e404b61be0bd3950f1ccf791f484fc87b0b
rockedpanda <chenghuawei@gmail.com>
2018-03-16 13:37:06 +0800

    增加注释说明和README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # pinyin-sort
 Sort by pinyin depend on GBK charactor index, for NodeJS
+
+利用GBK编码顺序实现简单的按照拼音排序的功能(未处理多音字)
+
+## API
+compare 按照拼音比较字符串顺序
+```javascript
+/**
+ * 根据拼音进行字符串比较
+ * pinyinOrder.compare(string1, string2)
+ * @param {String} string1 待比较的字符串1
+ * @param {String} string2 待比较的字符串2
+ * @return 1表示大于,0表示等于,-1表示小于
+ */
+const pinYinOrder = require('pinyin-order');
+console.log(pinYinOrder.compare('往', '高中') === 1);  //true
+console.log(pinYinOrder.compare('gao', '高')  === -1); //true
+```
+
+sort 按照拼音对字符串数组进行排序
+```javascript
+/**
+ * 根据字符串得到按拼音排序后的新字符串
+ * pinYinOrder.sort(list)
+ * @param {Array} list 待排序字符串数组
+ * @return Array 排序后的新数组
+ */
+const pinYinOrder = require('pinyin-order');
+var orderedList = pinYinOrder.sort(['李四','王五','张三','cheng','程']);
+console.log(orderList); //['cheng','程','李四','王五','张三']
+```
+
--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-var iconv = require('iconv-lite');
+/**
+ * 利用GBK编码顺序实现字符串按照拼音排序
+ */
+const iconv = require('iconv-lite');
 
-/*var gbkList = list.map(function(x,i){
-    var g = iconv.encode(x,'gbk');
-    return {index:i,t:g};
-});
-
-var utf8List = list.map(function (x, i) {
-    var g = iconv.encode(x, 'utf8');
-    return { index: i, t: g };
-});
-
-console.log(gbkList);
-console.log(utf8List);
-*/
-
+/**
+ * 根据拼音进行字符串比较
+ * @param {String} a 待比较的字符串1
+ * @param {String} b 待比较的字符串2
+ * @return 1表示大于,0表示等于,-1表示小于
+ */
 function gbkCompare(a, b) {
     function compareChar(a, b) {
         let buf1 = iconv.encode(a, 'gbk');
         let buf2 = iconv.encode(b, 'gbk');
-        //let ans = parseInt(buf1.toString('hex'), 16) - parseInt(buf2.toString('hex'), 16);
-        //console.log(a, b, ans, buf1.compare(buf2));
         return buf1.compare(buf2);
     }
     for (let i = 0; i < a.length; i++) {
@@ -34,6 +27,11 @@ function gbkCompare(a, b) {
     return -1;
 }
 
+/**
+ * 根据字符串得到按拼音排序后的新字符串
+ * @param {Array} list 待排序字符串数组
+ * @return Array 排序后的新数组
+ */
 function gbkSort(list){
     return list.sort(gbkCompare);
 }
